8f967db7750e824641fef3541ba09e489618c9bc
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-10-18 23:21:49 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numbers
 numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                [0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
                [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
@@ -12,6 +13,9 @@ numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 rnum = len(numbers)
 cnum = len(numbers[0])
 
-# ******************************
-# Make your Code
-# ******************************
+count = 0
+for i in range(1, rnum-1): 
+    for j in range(1, cnum-1):
+        if (numbers[i][j] == 1) and (numbers[i-1][j]== 1) and (numbers[i+1][j == 1]) and (numbers[i][j+1] == 1):
+            count += 1
+print(count)
